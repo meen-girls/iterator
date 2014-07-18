3dc8aa4b033bcdc3e7d40119d9c08f8beef03780
NickMele <nickmele26@gmail.com>
2014-07-17 23:27:30 -0400

    created a deep map function

--- index.js
+++ index.js
@@ -56,6 +56,65 @@ var iterateObject = function(obj, path, callback) {
   });
 };
 
+/**
+ * ### .deepMap(obj, path, callback)
+ *
+ * Iterate over an object and provide a callback function for all values returning an array
+ *
+ * ```js
+ * var obj = {
+ *     prop1: {
+ *         arr: ['a', 'b', 'c']
+ *       , str: 'Hello'
+ *     }
+ *   , prop2: {
+ *         arr: [ { nested: 'Universe' } ]
+ *       , str: 'Hello again!'
+ *     }
+ * };
+ *
+ * var paths = [];
+ * iterateObject(obj, function(key, path, value) {
+ *   paths.push(path);
+ * });
+ * console.log(paths); // ['prop1.arr[0]', 'prop1.arr[1]', 'prop1.arr[2]', ...]
+ * ```
+ *
+ * @param {Object} object
+ * @param {String} path
+ * @param {Function} callback
+ * @return {Array}
+ */
+var deepMap = function(obj, callback, path) {
+  var results = [];
+  _.forEach(obj, function(value, key) {
+    var currentPath;
+    if (_.isArray(obj)) {
+      currentPath = path + '[' + key + ']';
+    } else {
+      currentPath = path ? path + '.' + key : key;
+    }
+    if (value && _.isArray(value)) {
+      _.forEach(value, function(item, index) {
+        var tempPath = currentPath + '[' + index + ']';
+        if (!_.isObject(item) && !_.isArray(item)) {
+          results.push(callback(index, tempPath, item));
+        } else {
+          results = results.concat(deepMap(item, callback, tempPath));
+        }
+      });
+    } else if (value && _.isObject(value)) {
+      results = results.concat(deepMap(value, callback, currentPath));
+    } else {
+      results.push(callback(key, currentPath, value));
+    }
+  });
+  // compact the results removing undefined and null
+  results = _.compact(results);
+  return results;
+};
+
 module.exports = {
-  iterateObject: iterateObject
+  iterateObject: iterateObject,
+  deepMap: deepMap
 };
--- test/index.js
+++ test/index.js
@@ -30,3 +30,37 @@ describe('iterateObject', function() {
     });
   });
 });
+
+describe('deepMap', function() {
+  it('key should always be a string', function() {
+    iterator.deepMap(obj, function(key, path, value) {
+      should.exist(key);
+    });
+  });
+  it('path should always be a string', function() {
+    iterator.deepMap(obj, function(key, path, value) {
+      should.exist(path);
+    });
+  });
+  it('value should always exist', function() {
+    iterator.deepMap(obj, function(key, path, value) {
+      should.exist(value);
+    });
+  });
+  var results = iterator.deepMap(obj, function(key, path, value) {
+    return {
+      key: key,
+      path: path,
+      value: value
+    };
+  });
+  it('results should be an array', function() {
+    results.should.be.an('array');
+  });
+  it('results should be an array of 5 items', function() {
+    results.should.have.length(5);
+  });
+  it('last item in results should have property value with cool', function() {
+    results[4].should.have.deep.property('value', 'cool');
+  });
+});
